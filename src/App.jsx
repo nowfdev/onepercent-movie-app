789dec74e14bb8c0227d2ad6bef9456572182f4b
nowfdev <nowf.dev@gmail.com>
2024-12-16 23:42:51 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header></Header>
+      </div>
+    </>
+  );
+}
+// First commit
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Header</h1>
+    </div>
+  );
+};
+
+export default Header;

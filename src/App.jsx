b1545246069a42b3b4786a760e4bf54ff032fc52
nowfdev <nowf.dev@gmail.com>
2025-02-10 23:55:19 +0700

    Complete MovieList section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MovieList from "./components/MovieList";
 
 function App() {
   const [movie, setMovie] = useState([]);
+  const [movieUpcomming, setMovieUpcoming] = useState([]);
 
   useEffect(() => {
     const fetchMovie = async () => {
@@ -15,12 +16,22 @@ function App() {
           Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
         },
       };
-      const url =
+      const url1 =
         "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
-      const response = await fetch(url, options);
-      const data = await response.json();
-      console.log(data);
+      const url2 =
+        "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
+
+      const [res1, res2] = await Promise.all([
+        fetch(url1, options),
+        fetch(url2, options),
+      ]);
+
+      const data1 = await res1.json();
+      const data2 = await res2.json();
+
+      setMovie(data1.results);
+      setMovieUpcoming(data2.results);
     };
     fetchMovie();
   }, []);
@@ -29,8 +40,8 @@ function App() {
       <div className="bg-black pb-10">
         <Header />
         <Banner />
-        <MovieList title={"Treding"} />
-        <MovieList title={"Recommendation"} />
+        <MovieList title={"Trending"} data={movie} />
+        <MovieList title={"Upcomming"} data={movieUpcomming} />
       </div>
     </>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,57 @@
 import PropTypes from "prop-types";
-const MovieList = ({ title }) => {
-  return <div className="text-white p-10 mb-10 text-lg">{title} </div>;
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 8,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 6,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+const MovieList = ({ title, data }) => {
+  return (
+    <div className="text-white p-10 mb-10 ">
+      <h2 className="uppercase text-xl mb-4">{title}</h2>
+      <Carousel responsive={responsive} className="flex items-center space-x-2">
+        {data.length > 0 &&
+          data.map((item) => (
+            <div key={item.id} className="w-[200px] h-[300px] relative group">
+              <div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
+                <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
+                <img
+                  src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`}
+                  alt={item.title}
+                  className="w-full h-full object-cover "
+                ></img>
+                <div className="absolute bottom-10 left-2">
+                  <p className="uppercase text-md">
+                    {item.title || item.original_title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </Carousel>
+    </div>
+  );
 };
 
 MovieList.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  data: PropTypes.array,
 };
 
 export default MovieList;

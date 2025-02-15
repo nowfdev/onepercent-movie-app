53935c47530445d6eb8c9b2d61092b78b1c8edf0
nowfdev <nowf.dev@gmail.com>
2025-02-15 18:10:52 +0700

    Complete search func & update popup modal searched movie

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,5 +1,41 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import YouTube from "react-youtube";
+import Modal from "react-modal";
+
+const opts = {
+  height: "390",
+  width: "640",
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+  },
+};
+
 const MovieSearch = ({ title, data }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [trailerKey, setTrailerKey] = useState("");
+
+  const handleTrailer = async (id) => {
+    setTrailerKey("");
+    try {
+      const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
+      };
+      const movieKey = await fetch(url, options);
+      const data = await movieKey.json();
+      setTrailerKey(data.results[0].key);
+      setModalIsOpen(true);
+    } catch (error) {
+      setModalIsOpen(false);
+      console.log(error);
+    }
+  };
   return (
     <div className="text-white p-10 mb-10">
       <h2 className="uppercase text-xl mb-4">{title}</h2>
@@ -8,7 +44,7 @@ const MovieSearch = ({ title, data }) => {
           <div
             key={item.id}
             className="w-[200px] h-[300px] relative group"
-            // onClick={() => handleTrailer(item.id)}
+            onClick={() => handleTrailer(item.id)}
           >
             <div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
               <div className="absolute top-0 left-0 w-full h-full bg-black/40"></div>
@@ -26,6 +62,27 @@ const MovieSearch = ({ title, data }) => {
           </div>
         ))}
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            position: "fixed",
+            zIndex: 9999,
+          },
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
+        contentLabel="Example Modal"
+      >
+        <YouTube videoId={trailerKey} opts={opts} />
+      </Modal>
     </div>
   );
 };
